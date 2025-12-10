5897d0f75d14941106cbd508e6e9066e4075eddc
Richard Westhaver <ellis@rwest.io>
2025-12-09 22:26:00 -0500

    init

---
+++ pkg.lisp
@@ -0,0 +1,25 @@
+;;; pkg.lisp --- BBDB
+
+;; Big Brother Data Base
+
+;;; Code:
+(in-package :user)
+
+(defpkg :bbdb-int
+  (:use :std-lisp)
+  (:export :*bbdb-packages*))
+
+(in-package :bbdb-int)
+
+(defparameter *bbdb-packages* nil)
+(setq *defpkg-hook* (lambda (x) (pushnew (package-name x) *bbdb-packages* :test 'string=)))
+
+(defpkg :bbdb/core
+  (:use :core-lisp)
+  (:export :*bbdb-server* :*bbdb-clients* :bbdb 
+   :bbdb-service :bbdb-service-error :bbdb-service-warning))
+           
+
+(defpkg :bbdb/proto
+  (:use :core-lisp :bbdb/core)
+  (:export :entity :person :place :organization :company))
